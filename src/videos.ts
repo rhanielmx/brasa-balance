a9ed6f69f553d22fa81b73b6caeb494e18cd5e19
Rhaniel Xavier <rhanielmag.xavier@gmail.com>
2026-07-08 00:17:56 -0300

    feat: atualizar vídeos e observações das classes

--- src/videos.ts
+++ src/videos.ts
@@ -1,22 +1,22 @@
 import type { Classe } from "./types"
 
 export const CLASS_VIDEOS: Record<Classe, string[]> = {
-  Billposter: ["xLB-hGWDvtE", "rGedjDnQ9cw"],
-  Ringmaster: [],
-  Elementor: [],
-  Psykeeper: [],
-  Jester: [],
+  Billposter: [],
+  Ringmaster: ["7W1dXHWpac4"],
+  Elementor: ["dF5aap8XC4c"],
+  Psykeeper: [],//["SGbIyabg1ig"],
+  Jester: ["BrfeKxybBQg"],
   Ranger: [],
   Knight: [],
-  Blade: [],
+  Blade: ["TWtQ3-I8KjI"],
 }
 
 export const CLASS_OBS: Record<Classe, string> = {
-  Billposter: "Billposter e Jester tiveram os itens statados manualmente com pergaminho de status pois foram as primeiras testadas. Demais classes usaram awake de 2 de status em todos os itens para simular.",
+  Billposter: "",
   Ringmaster: "Todos os itens que são possíveis statar tem 1 linha extra de awake +2 INT ou +2 VIG para simular os pergaminhos de status. Demoraria muito ficar statando todos os itens manualmente.",
   Elementor: "Todos os itens que são possíveis statar tem 1 linha extra de awake +2 INT ou +2 VIG para simular os pergaminhos de status. Demoraria muito ficar statando todos os itens manualmente.",
   Psykeeper: "Todos os itens que são possíveis statar tem 1 linha extra de awake +2 INT ou +2 VIG para simular os pergaminhos de status. Demoraria muito ficar statando todos os itens manualmente.",
-  Jester: "Jester e Billposter tiveram os itens statados manualmente com pergaminho de status pois foram as primeiras testadas. Demais classes usaram awake de 2 de status em todos os itens para simular.",
+  Jester: "",
   Ranger: "Todos os itens que são possíveis statar tem 1 linha extra de awake +2 DEX ou +2 VIG para simular os pergaminhos de status. Demoraria muito ficar statando todos os itens manualmente.",
   Knight: "Todos os itens que são possíveis statar tem 1 linha extra de awake +2 FOR ou +2 VIG para simular os pergaminhos de status. Demoraria muito ficar statando todos os itens manualmente.",
   Blade: "Todos os itens que são possíveis statar tem 1 linha extra de awake +2 FOR ou +2 VIG para simular os pergaminhos de status. Demoraria muito ficar statando todos os itens manualmente.",
